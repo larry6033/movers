98fbaeee967a8c8b95ad8a625d274fb5fa1b882f
larry <larrymwimali@gmail.com>
2024-03-19 10:43:59 +0300

    added an extra page

--- js/form.js
+++ js/form.js
@@ -1,35 +1,90 @@
 const emailError = document.querySelector(".email__error");
 const nameError = document.querySelector(".name__error");
-console.log(emailError);
+console.log(nameError);
 const phoneError = document.querySelector(".phone__error");
 const addressError = document.querySelector(".address__error");
 const locationError = document.querySelector(".location__error");
 const destinationError = document.querySelector(".destination__error");
 const vehicleError = document.querySelector(".vehicle__error");
 const messageError = document.querySelector(".message__error");
-console.log(messageError);
-console.log(vehicleError);
+const submit = document.querySelector("#phone__error");
+console.log(submit);
+// console.log(messageError);
+// console.log(vehicleError);
 
-nameError.addEventListener("keyup", function(e){
-    // alert('bitch');
-   });
+// nameError.addEventListener("keyup", function(){
+//    //  alert('bitch');
 
-function valiDateName(){
- let name = document.querySelector("#contact-name").value;
+// function valiDateName(){
+//  let name = document.querySelector("#contact-name").value;
 
- if(name.length == 0){
-    nameError.innerHTML = "name is required";
-    return false;
- }
- if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-    nameError.innerHTML = 'write full name';
-    return false;
- }
- nameError.innerHTML = 'valid';
- return true;
-}valiDateName ();
+//  if(name.length == 0){
+//     nameError.innerHTML = "name is required";
+//     return false;
+//  }
+//  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+//     nameError.innerHTML = 'write full name';
+//     return false;
+//  }
+//  nameError.innerHTML = 'valid';
+//  return true;
+// // console.log(nameError)
+// }
+// // valiDateName ();
+
+// });
 
 
+
+// cartSections.addEventListener("click", function(e){
+   // alert(hello)
+//    if(e.target.classList.contains("increment")){
+//      const currentQuantity = e.target.previousElementSibling;
+ 
+//      currentQuantity.textContent++;
+ 
+//      console.log("adding items ");
+//      updatePrices();
+//      getSubTotals();
+//    }else if(e.target.classList.contains("decrement")){
+//      const currentQuantity = e.target.nextElementSibling;
+//      if(currentQuantity.textContent>1){
+//      currentQuantity.textContent--;
+//      }
+//      console.log("remove items");
+//      updatePrices()
+//      getSubTotals();
+//    }
+//  })
+// let validateName = '';
+// for
+submit.addEventListener("click", function(){
+   alert(hello);
+
+
+function validateName(name){
+  let name = document.querySelector(".name__error").value;
+  if(name.length === 0){
+   nameError.innerHTML = 'name is required';
+   return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+   nameError.innerHTML = 'write full name';
+return false;
+  }
+  nameError.innerHTML = "valid";
+  return true;
+}
+validateName();
+
+function valiDatephone(){
+   let phone = document.getElementById("contact-name").value;
+
+}
+
+
+
+})  
 
 
 
---
+++ js/script.js
@@ -0,0 +1,42 @@
+// console.log(nameError);
+
+// const locationError = document.querySelector(".location__error");
+console.log(locationError);
+
+const nameError = document.querySelector(".name__error");
+console.log(nameError);
+
+
+
+
+// locationError.addEventListener("click", function(e){
+//     // alert(hello)
+//     let location = document.querySelector("#contact-name").value;
+
+//     if(location.length == 0){
+//         locationErrorError.innerHTML = "location is required";
+//         return false;
+//      }
+//      if(!location.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+//         locationError.innerHTML = 'write location';
+//         return false;
+//      }
+//      locationError.innerHTML = 'valid';
+//      return true;
+    
+    
+//   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
